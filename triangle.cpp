13a1035138cf212af2481d4d1dba7ad5e696a32a
David Young-chan Kay <davidykay@gmail.com>
2018-09-17 12:26:12 -0700

    make a gradient. sexy

--- triangle.cpp
+++ triangle.cpp
@@ -36,9 +36,12 @@ bool init_resources(void) {
 		//"#version 100\n"  // OpenGL ES 2.0
 		"#version 120\n"  // OpenGL 2.1
 		"void main(void) {        "
-		"  gl_FragColor[0] = 0.0; "
-		"  gl_FragColor[1] = 0.0; "
-		"  gl_FragColor[2] = 1.0; "
+		//"  gl_FragColor[0] = 0.0; "
+		//"  gl_FragColor[1] = 0.0; "
+		//"  gl_FragColor[2] = 1.0; "
+    "  gl_FragColor[0] = gl_FragCoord.x/640.0; "
+    "  gl_FragColor[1] = gl_FragCoord.y/480.0; "
+    "  gl_FragColor[2] = 0.5; "
 		"}";
 	glShaderSource(fs, 1, &fs_source, NULL);
 	glCompileShader(fs);
