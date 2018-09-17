850d7a9d7b8bc69035e022d94a97a30d0108996d
David Young-chan Kay <davidykay@gmail.com>
2018-09-17 14:14:49 -0700

    About to implement stride

--- triangle.cpp
+++ triangle.cpp
@@ -20,7 +20,84 @@ GLuint64 program;
 GLuint vbo_triangle, vbo_triangle_colors;
 GLint attribute_coord2d, attribute_v_color;
 
+static const GLfloat g_vertex_buffer_data[] = {
+                                               -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+                                               -1.0f,-1.0f, 1.0f,
+                                               -1.0f, 1.0f, 1.0f, // triangle 1 : end
+                                               1.0f, 1.0f,-1.0f, // triangle 2 : begin
+                                               -1.0f,-1.0f,-1.0f,
+                                               -1.0f, 1.0f,-1.0f, // triangle 2 : end
+                                               1.0f,-1.0f, 1.0f,
+                                               -1.0f,-1.0f,-1.0f,
+                                               1.0f,-1.0f,-1.0f,
+                                               1.0f, 1.0f,-1.0f,
+                                               1.0f,-1.0f,-1.0f,
+                                               -1.0f,-1.0f,-1.0f,
+                                               -1.0f,-1.0f,-1.0f,
+                                               -1.0f, 1.0f, 1.0f,
+                                               -1.0f, 1.0f,-1.0f,
+                                               1.0f,-1.0f, 1.0f,
+                                               -1.0f,-1.0f, 1.0f,
+                                               -1.0f,-1.0f,-1.0f,
+                                               -1.0f, 1.0f, 1.0f,
+                                               -1.0f,-1.0f, 1.0f,
+                                               1.0f,-1.0f, 1.0f,
+                                               1.0f, 1.0f, 1.0f,
+                                               1.0f,-1.0f,-1.0f,
+                                               1.0f, 1.0f,-1.0f,
+                                               1.0f,-1.0f,-1.0f,
+                                               1.0f, 1.0f, 1.0f,
+                                               1.0f,-1.0f, 1.0f,
+                                               1.0f, 1.0f, 1.0f,
+                                               1.0f, 1.0f,-1.0f,
+                                               -1.0f, 1.0f,-1.0f,
+                                               1.0f, 1.0f, 1.0f,
+                                               -1.0f, 1.0f,-1.0f,
+                                               -1.0f, 1.0f, 1.0f,
+                                               1.0f, 1.0f, 1.0f,
+                                               -1.0f, 1.0f, 1.0f,
+                                               1.0f,-1.0f, 1.0f
+}
 
+// One color for each vertex. They were generated randomly.
+static const GLfloat g_color_buffer_data[] = {
+    0.583f,  0.771f,  0.014f,
+    0.609f,  0.115f,  0.436f,
+    0.327f,  0.483f,  0.844f,
+    0.822f,  0.569f,  0.201f,
+    0.435f,  0.602f,  0.223f,
+    0.310f,  0.747f,  0.185f,
+    0.597f,  0.770f,  0.761f,
+    0.559f,  0.436f,  0.730f,
+    0.359f,  0.583f,  0.152f,
+    0.483f,  0.596f,  0.789f,
+    0.559f,  0.861f,  0.639f,
+    0.195f,  0.548f,  0.859f,
+    0.014f,  0.184f,  0.576f,
+    0.771f,  0.328f,  0.970f,
+    0.406f,  0.615f,  0.116f,
+    0.676f,  0.977f,  0.133f,
+    0.971f,  0.572f,  0.833f,
+    0.140f,  0.616f,  0.489f,
+    0.997f,  0.513f,  0.064f,
+    0.945f,  0.719f,  0.592f,
+    0.543f,  0.021f,  0.978f,
+    0.279f,  0.317f,  0.505f,
+    0.167f,  0.620f,  0.077f,
+    0.347f,  0.857f,  0.137f,
+    0.055f,  0.953f,  0.042f,
+    0.714f,  0.505f,  0.345f,
+    0.783f,  0.290f,  0.734f,
+    0.722f,  0.645f,  0.174f,
+    0.302f,  0.455f,  0.848f,
+    0.225f,  0.587f,  0.040f,
+    0.517f,  0.713f,  0.338f,
+    0.053f,  0.959f,  0.120f,
+    0.393f,  0.621f,  0.362f,
+    0.673f,  0.211f,  0.457f,
+    0.820f,  0.883f,  0.371f,
+    0.982f,  0.099f,  0.879f
+};
 
 /**
  * Display compilation errors from the OpenGL shader compiler
@@ -111,9 +188,12 @@ bool init_resources(void) {
                                0.0, 0.0, 1.0,
                                1.0, 0.0, 0.0,
   };
-  glGenBuffers(1, &vbo_triangle_colors);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo_triangle_colors);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_colors), triangle_colors, GL_STATIC_DRAW);
+
+
+  GLuint colorbuffer;
+  glGenBuffers(1, &colorbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
   GLint compile_ok = GL_FALSE, link_ok = GL_FALSE;
 
@@ -130,7 +210,6 @@ bool init_resources(void) {
     cerr << "Error in glLinkProgram" << endl;
     return false;
   }
-
 
   //const std::string attribute_name = "v_color";
   const GLchar * attribute_name = "v_color";
@@ -161,6 +240,10 @@ void render(SDL_Window* window) {
                         0                  // offset of first element
                         );
 
+
+  // Draw the triangle !
+  //glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles -> 6 squares
+
   glEnableVertexAttribArray(attribute_coord2d);
   GLfloat triangle_vertices[] = {
                                  0.0,  0.8,
@@ -180,7 +263,7 @@ void render(SDL_Window* window) {
   /* Push each element in buffer_vertices to the vertex shader */
   glDrawArrays(GL_TRIANGLES, 0, 3);
 
-  glDisableVertexAttribArray(attribute_v_color);
+  // glDisableVertexAttribArray(attribute_v_color);
   glDisableVertexAttribArray(attribute_coord2d);
 
   /* Display the result */
